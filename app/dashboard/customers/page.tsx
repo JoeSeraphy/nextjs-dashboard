57dfa3340ed5eedb89a9408444d9b87d8f29a843
joelson Silva <joelson.s@outlook.com>
2024-07-13 17:41:20 -0300

    curso do nextjs Dashboard finalizado

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,6 +1,17 @@
 
+import CustomersTable from '@/app/ui/customers/table';
+import { Metadata } from 'next';
+
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
 export default function Page() {
   return (
-    <p>customers page</p>
+    <main>
+     <CustomersTable
+     customers={[]}
+     />
+    </main>
   )
 }
